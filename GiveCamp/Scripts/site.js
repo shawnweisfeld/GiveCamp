b5009ebaeab166fd54e7be6a25bd7614082324ca
Shawn Weisfeld <shawn@shawnweisfeld.com>
2013-04-14 12:20:12 -0500

    added countdown js

---
+++ GiveCamp/Scripts/site.js
@@ -0,0 +1,7 @@
+﻿function updateCharactersLeftTextBlock(textblock, messageDiv) {
+    var usedCharacters = $(textblock).val().length;
+    var totalCharacters = $(textblock).attr('data-limit');
+    var unusedCharacters = totalCharacters - usedCharacters;
+
+    $(messageDiv).html(unusedCharacters + " characters left");
+}
