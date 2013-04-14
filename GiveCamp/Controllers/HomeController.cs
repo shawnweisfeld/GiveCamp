be493cbdd8606b385d162d9b67d9bce37ec9b773
Shawn Weisfeld <shawn@shawnweisfeld.com>
2013-04-14 10:12:09 -0500

    home page cleanup and starting on contact us page

--- GiveCamp/Controllers/HomeController.cs
+++ GiveCamp/Controllers/HomeController.cs
@@ -24,8 +24,6 @@ namespace GiveCamp.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
     }
---
+++ GiveCamp/ViewModels/Home/ContactViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GiveCamp.ViewModels.Home
+{
+    public class ContactViewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Website { get; set; }
+        public string Subject { get; set; }
+        public string Message { get; set; }
+    }
+}
