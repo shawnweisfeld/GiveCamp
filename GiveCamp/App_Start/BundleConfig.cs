f571071f1730c03d4a6be66c50885ea108478ef8
Shawn Weisfeld <shawn@shawnweisfeld.com>
2013-04-14 12:42:57 -0500

    changed to use simplycountable vs custom js

--- GiveCamp/App_Start/BundleConfig.cs
+++ GiveCamp/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace GiveCamp
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jquery.simplyCountable.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
--- GiveCamp/Scripts/site.js
+++
@@ -1,7 +0,0 @@
-﻿function updateCharactersLeftTextBlock(textblock, messageDiv) {
-    var usedCharacters = $(textblock).val().length;
-    var totalCharacters = $(textblock).attr('data-limit');
-    var unusedCharacters = totalCharacters - usedCharacters;
-
-    $(messageDiv).html(unusedCharacters + " characters left");
-}
